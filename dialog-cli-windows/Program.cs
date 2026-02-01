724007beedbcfea206221194ff7924c553513c65
jurrejan <jurrejan@gmail.com>
2026-02-01 20:08:00 +0700

    fix: add missing DialogCLI.Components using in Program.cs

--- dialog-cli-windows/Program.cs
+++ dialog-cli-windows/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Windows;
+using DialogCLI.Components;
 using DialogCLI.Dialogs;
 using DialogCLI.Models;
 using DialogCLI.Services;
